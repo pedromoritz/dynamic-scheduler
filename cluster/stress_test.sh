f6d01edd15ac1314662432199e330b9a4627ded2
Pedro Moritz de Carvalho Neto <Pedro.MoritzdeCarvalhoNeto@windriver.com>
2022-05-29 14:47:46 -0300

    update

--- cluster/stress_test.sh
+++ cluster/stress_test.sh
@@ -7,14 +7,14 @@ kubectl create namespace lab1
 
 echo "CREATING PODS..."
 # quantidade de pods via parametro
-kubectl apply -f ../pods/stress-pod-ppgcc-1.yaml
-kubectl apply -f ../pods/stress-pod-ppgcc-2.yaml
-kubectl apply -f ../pods/stress-pod-ppgcc-3.yaml
+#kubectl apply -f ../pods/stress-pod-ppgcc-1.yaml
+#kubectl apply -f ../pods/stress-pod-ppgcc-2.yaml
+#kubectl apply -f ../pods/stress-pod-ppgcc-3.yaml
 
 kubectl get pods --namespace lab1 -o wide
 kubectl top nodes
 
-#hey -n 1000 -c 1 -o csv http://192.168.59.117:31002/ping > $1.csv
+#hey -n 10000 -c 1 -o csv  http://192.168.59.117:31001/memory/increase > $1.csv
 
 # modificações no hey:
 # exibir timestamp no csv 
--- scheduler/ppgcc_balancer.py
+++ scheduler/ppgcc_balancer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-#import time
-#import random
 import json
 
 from kubernetes import client, config, watch
@@ -13,7 +11,7 @@ v1 = client.CoreV1Api()
 scheduler_name = "ppgcc_balancer"
 namespace = "lab1"
 best_node = {}
-#bad_node = {}
+bad_node = {}
 
 def get_available_nodes():
   ready_nodes = []
@@ -30,43 +28,22 @@ def get_available_nodes():
   return ready_nodes
 
 def pod_scheduler(name, node, namespace="default"):
-  target = client.V1ObjectReference(kind = 'Node', api_version = 'v1', name = node)
-  meta = client.V1ObjectMeta(name = name)
-  body = client.V1Binding(target = target, metadata = meta)
+  target = client.V1ObjectReference(kind='Node', api_version='v1', name=node)
+  meta = client.V1ObjectMeta(name=name)
+  body = client.V1Binding(target=target, metadata=meta)
   try:
-    client.CoreV1Api().create_namespaced_binding(namespace=namespace, body=body)
-  except ValueError:
-    print("error")
+    v1.create_namespaced_binding(namespace=namespace, body=body, _preload_content=False)
+  except Exception as a:
+    print ("Exception when calling CoreV1Api->create_namespaced_binding: %s\n" % a)
 
-# def pod_evictor(name, namespace="default"):
-#     body = client.V1beta1Eviction(metadata=client.V1ObjectMeta(name=name, namespace=namespace))
-#     try:
-#         v1.create_namespaced_pod_eviction(name=name, namespace=namespace, body=body)
-#     except ValueError:
-#         print("error")
-
-# def get_bad_node(metrics_items):
-#   higher_node = sorted(metrics_items, key=lambda d: d['memory'], reverse=True)[0]
-#   return higher_node if int(higher_node['memory']) > 2000000 else {}
-
-# def get_target_pods_on_bad_node(bad_node):
-#     pods = []    
-#     if len(bad_node) > 0:
-#         global scheduler_name
-#         field_selector = 'spec.nodeName='+bad_node['name']+','+'metadata.namespace=lab1'+','+'spec.schedulerName='+scheduler_name
-#         list = v1.list_pod_for_all_namespaces(watch=False, field_selector=field_selector)
-
-#         for item in list.items:
-#             pod = {}
-#             pod['name'] = item.metadata.name
-#             pod['namespace'] = item.metadata.namespace
-#             pods.append(pod)
-
-#     return pods
+def pod_evictor(name, namespace="default"):
+  body = client.V1beta1Eviction(metadata=client.V1ObjectMeta(name=name, namespace=namespace))
+  try:
+    v1.create_namespaced_pod_eviction(name=name, namespace=namespace, body=body)
+  except Exception as a:
+    print ("Exception when calling CoreV1Api->create_namespaced_pod_eviction: %s\n" % a)
 
 def get_metrics():
-  global best_node
-#     global bad_node
   available_nodes = get_available_nodes()
   api_client = client.ApiClient()
   ret_metrics = api_client.call_api('/apis/metrics.k8s.io/v1beta1/nodes', 'GET', auth_settings=['BearerToken'], response_type='json', _preload_content=False)
@@ -84,15 +61,10 @@ def get_metrics():
     node['usage'] = {}
     node['usage']['memory'] = metrics_items[node['name']]['memory']
     node['usage']['cpu'] = metrics_items[node['name']]['cpu']
-  best_node = get_best_node(available_nodes)
   return available_nodes
 
 def get_best_node(available_nodes):
   return get_best_memory_node(available_nodes)
-#     target_pods_on_bad_node = get_target_pods_on_bad_node(bad_node)
-#     if len(target_pods_on_bad_node) > 0:
-#         pod_to_evict = target_pods_on_bad_node[0]
-#         pod_evictor(pod_to_evict['name'], pod_to_evict['namespace'])
 
 def get_best_memory_node(available_nodes):
   return sorted(available_nodes, key=lambda d: d['usage']['memory'])[0]
@@ -102,26 +74,45 @@ def scheduling_workflow(object, best_node_name, namespace):
   pod_scheduler(object.metadata.name, best_node_name, namespace)
 
 def eviction_workflow():
-  get_metrics()
+  bad_node = get_bad_node(get_metrics())
+  pods_to_evict = get_target_pods_on_node(bad_node)
+  if len(pods_to_evict) > 0:
+    pod_to_evict = pods_to_evict[0]
+    pod_evictor(pod_to_evict['name'], pod_to_evict['namespace'])
+
+def get_bad_node(available_nodes):
+  return get_worst_memory_node(available_nodes)
+
+def get_worst_memory_node(available_nodes):
+  higher_node = sorted(available_nodes, key=lambda d: d['usage']['memory'], reverse=True)[0]
+  return higher_node if int(higher_node['usage']['memory']) > 2000000 else {}
+
+def get_target_pods_on_node(bad_node):
+  pods = []
+  if len(bad_node) > 0:
+    global scheduler_name
+    field_selector = 'spec.nodeName='+bad_node['name']+','+'metadata.namespace=lab1'+','+'spec.schedulerName='+scheduler_name
+    pods_list = v1.list_pod_for_all_namespaces(watch=False, field_selector=field_selector)
+    for item in pods_list.items:
+      pod = {}
+      pod['name'] = item.metadata.name
+      pod['namespace'] = item.metadata.namespace
+      pods.append(pod)
+  return pods
 
 def main():
-  # retrieving metrics for the first time
-  metrics = get_metrics()
-  #print(metrics)
   # creating a scheduler to query metrics server every x seconds
   sch = scheduler()
-  sch.add_job(eviction_workflow, 'interval', seconds=10)
+  sch.add_job(eviction_workflow, 'interval', seconds=5)
   sch.start()
   # creating a watch to verify pending pods and binding them to a node
   w = watch.Watch()
   for event in w.stream(v1.list_namespaced_pod, namespace):
-    get_metrics()
-    if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name:
+    if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name and event['object'].spec.node_name == None:
       try:
-        scheduling_workflow(event['object'], best_node['name'], namespace)
+        scheduling_workflow(event['object'], get_best_node(get_metrics())['name'], namespace)
       except client.rest.ApiException as e:
-        print('')
-        #print(json.loads(e.body)['message'])
+        print(json.loads(e.body)['message'])
 
 if __name__ == '__main__':
   main()
