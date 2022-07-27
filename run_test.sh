2e856cbf65add9fb7b27a8b4dbc18a5b0cd3c690
Pedro Moritz de Carvalho Neto <Pedro.MoritzdeCarvalhoNeto@windriver.com>
2022-07-26 22:50:48 -0300

    update

--- run_test.sh
+++ run_test.sh
@@ -8,13 +8,28 @@ if [[ " ${options[@]} " =~ " $1 " ]]; then
 	kubectl create namespace lab1
 
 	echo "CREATING PODS..."
-	kubectl apply -f pods/pod-$1.yaml
+	for i in 1 2 3 4 5; do	
+		POD_NAME=pod-$1-$i
+		NODE_PORT=3100$i
+		CUSTOM_SCHEDULER=""
+
+		if [[ "dynamic-scheduler" == "$1" ]]; then
+			CUSTOM_SCHEDULER="schedulerName: dynamic-scheduler"
+		fi
+
+		template=`cat "pods/pod-deployment-template.yaml" | sed "s/{{POD_NAME}}/$POD_NAME/g"`
+		template=`echo "$template" | sed "s/{{NODE_PORT}}/$NODE_PORT/g"`
+		template=`echo "$template" | sed "s/{{CUSTOM_SCHEDULER}}/$CUSTOM_SCHEDULER/g"`
+		echo "$template" | kubectl apply -f -
+	done
 
 	sleep 20
 
 	echo "INCREASING MEMORY..."
-	#hey -n 10000 -c 10 -q 10 -o csv http://192.168.59.121:31001/memory/increase > csv/$1.csv
-	hey -n 10000 -c 1 http://192.168.59.121:31001/memory/increase
+	for i in 1 2 3 4 5; do
+  		hey -n 10000 -c 1 http://192.168.59.121:3100$i/memory/increase &
+	   #hey -n 10000 -c 10 -q 10 -o csv http://192.168.59.121:31001/memory/increase > csv/$1.csv
+  	done
 else
 	echo "usage:"
 	echo "./run_test.sh kube-scheduler or"
---
+++ src/dynamic-scheduler-core.py
@@ -0,0 +1,7 @@
+class DynamicSchedulerCore:
+
+    @staticmethod
+    def addNumbers(x, y):
+        return x + y
+
+print('Product:', DynamicSchedulerCore.addNumbers(15, 110))
