c5a9cbbbcb1c8470ede8dcd8657c8e7a609f1acd
Pedro Moritz de Carvalho Neto <Pedro.MoritzdeCarvalhoNeto@windriver.com>
2022-08-30 19:08:19 -0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+import json
 import dynamic_scheduler_core as dscore
 
 cluster = dscore.Cluster()
 
-print(cluster.nodes)
+for node_item in cluster.nodes:
+	node = dscore.Node(node_item['name'])
+	print(node.metrics)
+
+print(json.dumps(cluster.nodes))
