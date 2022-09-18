96135b234b61e4ec5c5db05364967fc2e32a8c67
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2022-09-18 18:26:13 -0300

    update

--- main_1.py
+++ main_1.py
@@ -14,16 +14,16 @@ def scheduling_workflow():
   print('')
 
   cluster = dscore.Cluster()
-  print('---> Buscando nodes do cluster:')
+  #print('---> Buscando nodes do cluster:')
   nodes = cluster.nodes
-  print(nodes)
-  print('')
+  #print(nodes)
+  #print('')
 
   for node_item in nodes:
     node = dscore.Node(node_item['name'], namespace)
-    print('---> Buscando metricas para o node ' + node_item['name'] + ':')
-    print(node.metrics)
-    print('')
+    #print('---> Buscando metricas para o node ' + node_item['name'] + ':')
+    #print(node.metrics)
+    #print('')
     node_item['usage'] = node.metrics['usage']
 
   print('---> Nodes com respectivas metricas:')
@@ -36,43 +36,51 @@ def scheduling_workflow():
   print(node_more_used_memory)
   print('')
 
-  print('---> Buscando pods do node ' + node_more_used_memory['name'] + ':')
-  node = dscore.Node(node_more_used_memory['name'], namespace)
-  print(node.pods)
-  print('')
+  print('---> Verificando se o node selecionado possui ao menos 70% de uso de memória:')
+  percentual_memory_usage = (node_more_used_memory['usage']['memory'] / node_more_used_memory['capacity']['memory']) * 100
+  print(round(percentual_memory_usage, 2))
 
-  print('---> Verifica Se há ao menos um pod no node selecionado:')
-  print('')
-  if len(node.pods) > 0: 
+  if percentual_memory_usage >= 70:
 
-    print('---> Buscando primeiro pod da lista:')
-    pod_to_evict = node.pods[0]
-    print(pod_to_evict)
+    print('---> Buscando pods do node ' + node_more_used_memory['name'] + ':')
+    node = dscore.Node(node_more_used_memory['name'], namespace)
+    print(node.pods)
     print('')
 
-    print('---> Despejando pod ' + pod_to_evict['name'] + ' do respectivo node:')
-    pod = dscore.Pod(pod_to_evict['name'], pod_to_evict['namespace'])
-    pod.evict()
+    print('---> Verifica Se há ao menos um pod no node selecionado:')
     print('')
+    if len(node.pods) > 0: 
 
-    print('---> Buscando node com menor uso absoluto de memória:')
-    nodes_sorted_by_used_memory_reverse = sorted(nodes, key=lambda d: d['usage']['memory'], reverse=True)
-    node_less_used_memory = nodes_sorted_by_used_memory_reverse[-1]
-    print(node_less_used_memory)
-    print('')
+      print('---> Buscando primeiro pod da lista:')
+      pod_to_evict = node.pods[0]
+      print(pod_to_evict)
+      print('')
 
-    print('---> Alocando o pod ' + pod_to_evict['name'] + ' em um node específico:')
-    pod.schedule(node_less_used_memory['name'])
-    print('')
+      print('---> Despejando pod ' + pod_to_evict['name'] + ' do respectivo node:')
+      pod = dscore.Pod(pod_to_evict['name'], pod_to_evict['namespace'])
+      pod.evict()
+      print('')
 
-  else: 
+      print('---> Buscando node com menor uso absoluto de memória:')
+      nodes_sorted_by_used_memory_reverse = sorted(nodes, key=lambda d: d['usage']['memory'], reverse=True)
+      node_less_used_memory = nodes_sorted_by_used_memory_reverse[-1]
+      print(node_less_used_memory)
+      print('')
 
-    print('---> Não há pod a ser despejado no node selecionado!')
-    print('')
+      print('---> Alocando o pod ' + pod_to_evict['name'] + ' em um node específico:')
+      pod.schedule(node_less_used_memory['name'])
+      print('')
+
+    else: 
+
+      print('---> Não há pod a ser despejado no node selecionado!')
+      print('')
+
+scheduling_workflow()
 
 # creating a timer for workflow trigger
 scheduler = BackgroundScheduler()
-scheduler.add_job(scheduling_workflow, 'interval', seconds=20)
+scheduler.add_job(scheduling_workflow, 'interval', seconds=60)
 scheduler.start()
 
 # keeping script running
