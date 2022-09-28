1659b81260b0fd3be47cda68d800cd3d9b95ccf6
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2022-09-28 08:54:16 -0300

    update

--- experiments/seeding.sh
+++ experiments/seeding.sh
@@ -8,7 +8,7 @@ kubectl create namespace lab
 for i in 1 2 3 4 5 6; do	
 	POD_NAME=pod-$i
 	NODE_PORT=3100$i
-	template=`cat "pods/pod-deployment-template.yaml" | sed "s/{{POD_NAME}}/$POD_NAME/g"`
+	template=`cat "../pods/pod-deployment-template.yaml" | sed "s/{{POD_NAME}}/$POD_NAME/g"`
 	template=`echo "$template" | sed "s/{{NODE_PORT}}/$NODE_PORT/g"`
 	template=`echo "$template" | sed "s/{{CUSTOM_SCHEDULER}}/$CUSTOM_SCHEDULER/g"`
 	echo "$template" | kubectl apply -f -
