c3a6de512206ea45fea586e053ab88787b464612
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2022-11-03 23:26:51 -0300

    update

--- customapp/app.js
+++ customapp/app.js
@@ -1,11 +1,54 @@
 'use strict';
 
 const express = require('express');
-const inefficient = require('inefficient');
-
 const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express();
 
-app.get('/stress', inefficient);
+const memoryLeakAllocations = [];
+const allocationStep = 10000 * 1024;
+
+function allocateMemory(size) {
+  const numbers = size / 8;
+  const arr = [];
+  arr.length = numbers;
+  for (let i = 0; i < numbers; i++) {
+    arr[i] = i;
+  }
+  return arr;
+}
+
+function memoryPurge() {
+  memoryLeakAllocations.length = 0;
+  global.gc();
+}
+
+app.get('/memory/increase', (req, res) => {
+  const allocation = allocateMemory(allocationStep);
+  const memoryLeakAllocations = [];
+  memoryLeakAllocations.push(allocation);
+  const memoryUsage = process.memoryUsage();
+  const gbNow = memoryUsage['heapUsed'] / 1024 / 1024 / 1024;
+  const gbRounded = Math.round(gbNow * 100) / 100;
+ 
+  console.log(`1 -> Heap allocated ${gbRounded} GB\n`);
+  setTimeout(function() {
+    memoryLeakAllocations.length = 0;
+    global.gc();
+    console.log(`Deallocating memory...`);
+    res.send(`Heap allocated ${gbRounded} GB\n`);
+  }, 10000);
+  // console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+});
+
+app.get('/memory/purge', (req, res) => {
+  memoryPurge();
+  console.log('purge');
+  res.send('purge\n');
+});
+
+process.on('SIGINT', function() {
+  process.exit();
+});
+
 app.listen(PORT, HOST);
