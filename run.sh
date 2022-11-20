3410e8ffc7d51a9d16b16c8160a609e4ddb87654
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2022-11-20 19:53:26 -0300

    update

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: ./run.sh <SCHEDULER_TYPE: default-scheduler | dynamic-scheduler-GreedyLB> <POD_AMOUNT>"
+  exit 1
+fi
+
+# defining scheduler
+if [ "$1" = "dynamic-scheduler-GreedyLB" ]; then
+  CUSTOM_SCHEDULER="schedulerName: dynamic-scheduler"
+else
+  CUSTOM_SCHEDULER=""
+fi
+
+# removing all workloads
+kubectl delete namespace lab
+kubectl create namespace lab
+
+# creating workloads
+for i in $(seq $2); do	
+  POD_NAME=pod-$i
+  NODE_PORT=31$(printf %03d $i)
+  template=`cat "pod-deployment-template.yaml" | sed "s/{{POD_NAME}}/$POD_NAME/g"`
+  template=`echo "$template" | sed "s/{{NODE_PORT}}/$NODE_PORT/g"`
+  template=`echo "$template" | sed "s/{{CUSTOM_SCHEDULER}}/$CUSTOM_SCHEDULER/g"`
+  echo "$template" | kubectl apply -f -
+done
+
+# scheduling workloads for initial state (round robin)
+if [ "$1" = "dynamic-scheduler-GreedyLB" ]; then
+  ./round_robin_scheduler.py
+fi
+
+# waiting for ready containers
+sleep 30
+
+# scheduling workloads for initial state (round robin)
+if [ "$1" = "dynamic-scheduler-GreedyLB" ]; then
+  ./dynamic-scheduler-GreedyLB.py
+fi
+
+# starting testset
+k6 run -q --out csv=testcase-1_results.csv -e SCHEDULER_TYPE=$1 -e POD_AMOUNT=$2 k6_script.js &
+
--- testcase.sh
+++ testcase.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # defining scheduler
-CUSTOM_SCHEDULER="schedulerName: dynamic-scheduler"
+CUSTOM_SCHEDULER=""
 
 # removing all workloads
-kubectl delete pods --all -n default --grace-period 0 --force
+#kubectl delete pods --all -n lab --grace-period 0 --force
+kubectl delete namespace lab
+kubectl create namespace lab
 
 # creating workloads
-for i in 1 2 3 4 5 6; do	
+for i in $(seq $1); do	
 	POD_NAME=pod-$i
-	NODE_PORT=3100$i
+	NODE_PORT=31$(printf %03d $i)
 	template=`cat "pod-deployment-template.yaml" | sed "s/{{POD_NAME}}/$POD_NAME/g"`
 	template=`echo "$template" | sed "s/{{NODE_PORT}}/$NODE_PORT/g"`
 	template=`echo "$template" | sed "s/{{CUSTOM_SCHEDULER}}/$CUSTOM_SCHEDULER/g"`
@@ -17,14 +19,11 @@ for i in 1 2 3 4 5 6; do
 done
 
 # scheduling workloads for initial state (round robin)
-./round_robin_scheduler.py
+#./round_robin_scheduler.py
 
 # waiting for ready containers
 sleep 30
 
 # starting testset
-k6 run -q --out csv=testcase-2_results.csv k6_script-testcase-2.js &
-
-# starting dynamic scheduling
-./dynamic-scheduler-GreddyLB.py
+k6 run -q --out csv=testcase-1_results.csv -e SCHEDULER_TYPE=default_scheduler k6_script.js &
 
