0b55a56869d9d00e2d44e0b5bc24ade3b105faf8
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2022-11-21 00:25:34 -0300

    update

--- dynamic-scheduler-GreedyLB.py
+++ dynamic-scheduler-GreedyLB.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import json
 import time
 import k8s_helper_core as khc
 from apscheduler.schedulers.background import BackgroundScheduler
+from heapq import heappop, heappush, heapify
 
 def get_round_robin_plan(pods, nodes):
   allocation_plan = {} 
@@ -13,7 +13,54 @@ def get_round_robin_plan(pods, nodes):
     rr_counter = rr_counter + 1 if rr_counter < len(nodes) - 1 else 0
   return allocation_plan  
 
-#def get_greedylb_plan(chare_objects, processors, background_load):
+def get_greedylb_plan(chare_objects, processors, background_load):
+  for pod in chare_objects:
+    print(pod)
+
+  transformed_chare_objects = map(lambda n: [n['usage']['memory'], n['name']], chare_objects) 
+
+  heap = []
+  heapify(heap)
+  for pod in transformed_chare_objects:
+    heappush(heap, pod)
+print("The Head value of heap is : "+str(-1 * heap[0]))
+print("Heap elements are : ")
+for i in heap:
+    print(-1 * i, end = ' ')
+print("\n")
+element = heappop(heap)
+
+
+
+  for pod in transformed_chare_objects:
+    print(pod)
+
+  allocation_plan = {}
+  return allocation_plan
+#itens_na_mochila = []
+#peso_da_mochila = 0
+# ordenando a lista pela razão valor/peso do item
+#itens_ordenados = sorted(itens, key=lambda item: item[0]/item[1], reverse=True)
+# executando o algoritmo guloso
+#for item_ordenado in itens_ordenados:
+#peso_item_ordenado = item_ordenado[1]
+#if peso_item_ordenado + peso_da_mochila <= capacidade_mochila:
+#itens_na_mochila.append(item_ordenado)
+#peso_da_mochila += peso_item_ordenado
+#return itens_na_mochila
+# lista de items [valor, peso]
+#itens = [
+#[140, 10],
+#[120, 6],
+#[100, 2],
+#[80, 5],
+#[90, 5]
+#]
+# capacidade máxima de peso da mochila
+#capacidade_mochila = 13
+#itens_colocados_na_mochila = problema_da_mochila(itens, capacidade_mochila)
+#print(itens_colocados_na_mochila)
+
 
 # workflow definitions
 def scheduling_workflow():
@@ -49,33 +96,27 @@ def scheduling_workflow():
   percentual_delta = round((absolute_delta / node_more_used_memory['capacity']['memory']) * 100, 2)
   print('absolute delta: ' + str(absolute_delta) + 'KB | ' + 'percentual delta: ' + str(percentual_delta) + '%', end="\n\n")
 
-  if (percentual_delta > 20):
+  if (percentual_delta > 20 or True):
 
     all_pods = []
 
     for node_item in nodes:
-      print('---> Buscando pods do node ' + node_item['name'] + ':')
       node = khc.Node(node_item['name'])
-      pods_temp = node.pods
-
-      for pod_temp in pods_temp:
-        pod_temp['source_node'] = node_item['name']
-        all_pods.append(pod_temp)
-        print(pod_temp)
-
-      print('')
+      all_pods = all_pods + node.pods
 
     print('---> Criando uma lista de alocação')
-    allocation_plan = get_round_robin_plan(all_pods, nodes) 
+#    allocation_plan = get_round_robin_plan(all_pods, nodes) 
+    allocation_plan = get_greedylb_plan(all_pods, nodes, 1000)
 
     for allocation_plan_item in allocation_plan:
       print(allocation_plan_item)
 
-    cluster.set_allocation_plan(allocation_plan)
+#    cluster.set_allocation_plan(allocation_plan)
 
+scheduling_workflow()
 # creating a timer for workflow trigger
 scheduler = BackgroundScheduler()
-scheduler.add_job(scheduling_workflow, 'interval', seconds=30)
+scheduler.add_job(scheduling_workflow, 'interval', seconds=10)
 scheduler.start()
 
 # keeping script running
