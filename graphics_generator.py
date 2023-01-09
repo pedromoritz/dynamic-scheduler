d9ab56a907add6e7e582d165bdc0d3d7059ec3f0
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-01-08 23:22:33 -0300

    update

--- graphics_generator.py
+++ graphics_generator.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pylab import cm
 
-CSV_FILENAME = 'metrics_dynamic_scheduler_GreedyLB_memory.csv'
+CSV_FILENAME = 'metrics_dynamic_scheduler_GreedyLB_6_pods.csv'
 
-timestamp, node1, node2, node3 = np.loadtxt(CSV_FILENAME, unpack=True, delimiter=',', skiprows=0)
+#timestamp,ppgcc-m02,ppgcc-m02_pods,ppgcc-m03,ppgcc-m03_pods,ppgcc-m04,ppgcc-m04_pods
 
-def to_relative_time(n):
-  return n - timestamp[0]
+timestamp, node1 = np.loadtxt(CSV_FILENAME, unpack=True, delimiter=',', skiprows=1)
 
-relative_time = list(map(to_relative_time, timestamp))
-plt.plot(relative_time, node1)
-plt.savefig('Final_Plot.png', dpi=300, transparent=False, bbox_inches='tight')
+#def to_relative_time(n):
+#  return n - timestamp[0]
+
+#relative_time = list(map(to_relative_time, timestamp))
+#plt.plot(timestamp, node1)
+#plt.savefig('Final_Plot.png', dpi=300, transparent=False, bbox_inches='tight')
