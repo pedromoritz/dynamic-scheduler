fac43e5f4aed35518f9cc7c722f1360f711930ae
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-01-08 23:56:47 -0300

    update

--- graphics_generator.py
+++ graphics_generator.py
@@ -10,11 +10,16 @@ CSV_FILENAME = 'metrics_dynamic_scheduler_GreedyLB_6_pods.csv'
 
 #timestamp,ppgcc-m02,ppgcc-m02_pods,ppgcc-m03,ppgcc-m03_pods,ppgcc-m04,ppgcc-m04_pods
 
-timestamp, node1 = np.loadtxt(CSV_FILENAME, unpack=True, delimiter=',', skiprows=1)
+timestamp, m_node1, pa_node1, m_node2, pa_node2, m_node3, pa_node3 = np.loadtxt(CSV_FILENAME, unpack=True, delimiter=',', skiprows=1)
 
-#def to_relative_time(n):
-#  return n - timestamp[0]
+m_node1_mb = list(map(lambda n: n/1024, m_node1))
+m_node2_mb = list(map(lambda n: n/1024, m_node2))
+m_node3_mb = list(map(lambda n: n/1024, m_node3))
 
-#relative_time = list(map(to_relative_time, timestamp))
-#plt.plot(timestamp, node1)
-#plt.savefig('Final_Plot.png', dpi=300, transparent=False, bbox_inches='tight')
+plt.xlabel('time (s)')
+plt.ylim(0, 4096)
+plt.ylabel('memory (MB)')
+plt.plot(timestamp, m_node1_mb)
+plt.plot(timestamp, m_node2_mb)
+plt.plot(timestamp, m_node3_mb)
+plt.savefig(CSV_FILENAME+'.png', dpi=300, transparent=False, bbox_inches='tight')
