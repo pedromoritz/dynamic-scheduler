ab005029f8f02bebfa52c3ab9277ffa08de4a0cd
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-01-18 16:41:27 -0300

    update

--- k6_script.js
+++ k6_script.js
@@ -1,14 +1,21 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 import { reportHTML } from "https://raw.githubusercontent.com/fziviello/k6-report-html/main/dist/reportHtml.min.js";
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
   stages: [
-    { duration: '60s', target: 1000 },
+    { duration: '5m', target: parseInt(__ENV.VIRTUAL_USERS)},
   ],
 };
 
-const API_BASE_URL = 'http://192.168.59.104';
+const API_BASE_URL = 'http://192.168.59.104'
+
+const urls = []
+
+for (let i = 0; i < parseInt(__ENV.POD_AMOUNT); i++) {
+  urls.push(`${API_BASE_URL}:31${String(i+1).padStart(3, '0')}/memory/increase`)
+}
 
 export function handleSummary(data) {
   return {
@@ -17,11 +24,7 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  http.batch([
-    ['GET', `${API_BASE_URL}:31001/memory/increase`],
-    ['GET', `${API_BASE_URL}:31002/memory/increase`],
-    ['GET', `${API_BASE_URL}:31003/memory/increase`]
-  ]);
-
+  const url = urls[randomIntBetween(0, urls.length - 1)]
+  const res = http.get(url)
   sleep(1);
 }
