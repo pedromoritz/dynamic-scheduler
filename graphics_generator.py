9f9712fec446392acd286352e480ac1df2be5bfd
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-01-31 19:45:16 -0300

    update

--- graphics_generator.py
+++ graphics_generator.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 
-# Import required packages
+import os
+from pathlib import Path
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from pylab import cm
 import sys
 
-TEST_NAME = sys.argv[1]
+for path in Path("./").glob("metrics_*.csv"):
+  FILE_NAME, FILE_EXTENSION = os.path.splitext(path)
+  print("Reading " + str(path))
+  timestamp, m_node1, pa_node1, m_node2, pa_node2, m_node3, pa_node3 = np.loadtxt(FILE_NAME + '.csv', unpack=True, delimiter=',', skiprows=1)
 
-timestamp, m_node1, pa_node1, m_node2, pa_node2, m_node3, pa_node3 = np.loadtxt(TEST_NAME + '.csv', unpack=True, delimiter=',', skiprows=1)
+  m_node1_mb = list(map(lambda n: n/1024, m_node1))
+  m_node2_mb = list(map(lambda n: n/1024, m_node2))
+  m_node3_mb = list(map(lambda n: n/1024, m_node3))
 
-m_node1_mb = list(map(lambda n: n/1024, m_node1))
-m_node2_mb = list(map(lambda n: n/1024, m_node2))
-m_node3_mb = list(map(lambda n: n/1024, m_node3))
-
-plt.xlabel('time (s)')
-plt.ylim(0, 4096)
-plt.ylabel('memory (MB)')
-plt.plot(timestamp, m_node1_mb, label='node 1')
-plt.plot(timestamp, m_node2_mb, label='node 2')
-plt.plot(timestamp, m_node3_mb, label='node 3')
-plt.legend(loc="upper left")
-plt.savefig(TEST_NAME + '.png', dpi=300, transparent=False, bbox_inches='tight')
+  plt.xlabel('time (s)')
+  plt.ylim(0, 4096)
+  plt.ylabel('memory (MB)')
+  plt.plot(timestamp, m_node1_mb, label='node 1')
+  plt.plot(timestamp, m_node2_mb, label='node 2')
+  plt.plot(timestamp, m_node3_mb, label='node 3')
+  plt.legend(loc="upper left")
+  print("Generating " + FILE_NAME + ".png")
+  plt.savefig(FILE_NAME + '.png', dpi=300, transparent=False, bbox_inches='tight')
--- run_kse-GreedyLB.sh
+++ run_kse-GreedyLB.sh
@@ -29,13 +29,14 @@ test()
   # waiting for ready containers
   sleep 60
 
+  # retrieving service IP
+  SVCIP=`minikube ip -p ppgcc`
+  
   # starting testset
-  k6 run -q --out csv="results_${ST}_${PA}_${VU}.csv" -e SCHEDULER_TYPE=$ST -e POD_AMOUNT=$PA -e VIRTUAL_USERS=$VU k6_script.js >/dev/null 2>&1 &
+  k6 run -q --out csv="results_${ST}_${PA}_${VU}.csv" -e SVC_IP=$SVCIP -e SCHEDULER_TYPE=$ST -e POD_AMOUNT=$PA -e VIRTUAL_USERS=$VU k6_script.js >/dev/null 2>&1 &
 
   # metrics monitoring
   ./kse-GreedyLB.py $ST $PA $VU
-
-  kubectl delete namespace lab
 }
 
 test kse-GreedyLB $1 $2
