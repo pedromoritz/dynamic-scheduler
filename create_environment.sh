4354644ca8a0d8dc287d4d83dbf2e57f8654dc45
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-02-18 17:45:31 -0300

    update

--- create_environment.sh
+++ create_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 minikube stop -p ppgcc
 minikube delete -p ppgcc
-minikube start --nodes 4 -p ppgcc --cpus 2 --memory 4096 --disk-size 10G --vm --kubernetes-version v1.25.3
+minikube start --nodes 4 -p ppgcc --cpus 2 --memory 4096 --disk-size 5G --vm --kubernetes-version v1.25.3
 minikube addons enable metrics-server -p ppgcc
 
