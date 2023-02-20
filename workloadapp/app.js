acdc4f31ab203e2b3d8e337f5b10782f27241ce9
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-02-20 18:53:43 -0300

    update

--- workloadapp/app.js
+++ workloadapp/app.js
@@ -18,7 +18,7 @@ app.get('/do', (req, res) => {
 
   let array = [];
 
-  for (let i = 0; i < 300000; i++) {
+  for (let i = 0; i < 500000; i++) {
     array.push(sha512((Date.now() + Math.random()).toString()).toString());
   }
 
