f53023170d9efa63533ba04ae73d1ca6e866d25e
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-03-17 16:44:15 -0300

    update

--- workloadapp/app.js
+++ workloadapp/app.js
@@ -6,13 +6,17 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 const app = express();
 
+function showHeapUsage(step) {
+  let memoryUsage = process.memoryUsage();
+  let headUsed = `${Math.round(memoryUsage['heapUsed'] / 1024 / 1024 * 100) / 100}`;
+  console.log(`Heap allocated ${step}: ${headUsed} MB\n`);
+}
+
 app.get('/do', (req, res) => {
-  let array = [];
-
-  for (let i = 0; i < 100000; i++) {
-    array.push(sha512(Math.random().toString()).toString());
-  }
-
+  showHeapUsage('before');
+  const array = Array.from({length: 1000000}, () => Math.random() * 10);
+  //const arr = new Array(10000000).fill(1);
+  showHeapUsage('after');
   res.send('done');
 });
 
