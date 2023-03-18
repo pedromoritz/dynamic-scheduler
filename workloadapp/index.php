675d9134f4d8cfb359925433d441b9fba88cd17c
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-03-18 10:57:20 -0300

    update

--- workloadapp/index.php
+++ workloadapp/index.php
@@ -1,11 +1,6 @@
 <?php
-ini_set('memory_limit', '1024M');
-
 $array = array();
-
 foreach (range(0, 100000) as $number) {
 	array_push($array, $number);
 }
-
-echo '<pre>'.count($array).'</pre>';
 ?>
