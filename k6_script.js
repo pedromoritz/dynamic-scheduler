2f5827eb2c9c39c349c8e85df479f43fd5529a5c
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-03-26 17:39:46 -0300

    update

--- k6_script.js
+++ k6_script.js
@@ -1,13 +1,20 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
 import { reportHTML } from "https://raw.githubusercontent.com/fziviello/k6-report-html/main/dist/reportHtml.min.js";
-import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import { getNormallyDistributedRandomNumber } from './pdf.js';
+import { sleep } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '30m', target: parseInt(__ENV.VIRTUAL_USERS)},
-  ],
+  discardResponseBodies: true,
+  scenarios: {
+    contacts: {
+      executor: 'ramping-vus',
+      startVUs: 0,
+      stages: [
+        { duration: '30m', target: __ENV.VIRTUAL_USERS },
+      ],
+      gracefulRampDown: '0s',
+    },
+  },
 };
 
 const API_BASE_URL = `http://${__ENV.SVC_IP}`
@@ -20,13 +27,13 @@ for (let i = 0; i < parseInt(__ENV.POD_AMOUNT); i++) {
 
 export function handleSummary(data) {
   return {
-    [`results/summary_${__ENV.SCHEDULER_TYPE}_${__ENV.POD_AMOUNT}_${__ENV.VIRTUAL_USERS}.html`]: reportHTML(data, {title:`${__ENV.SCHEDULER_TYPE} - ${__ENV.POD_AMOUNT} pods - ${__ENV.VIRTUAL_USERS} virtual users`})
+    [`results/summary_${__ENV.SCHEDULER_TYPE}_${__ENV.POD_AMOUNT}_${__ENV.VIRTUAL_USERS}.html`]: reportHTML(data, {title:`${__ENV.SCHEDULER_TYPE} - ${__ENV.POD_AMOUNT} pods - ${__ENV.VIRTUAL_USERS} maximun virtual users`})
   };
 }
 
 export default function () {
-  //const url = urls[randomIntBetween(0, urls.length - 1)]
   const url = urls[getNormallyDistributedRandomNumber(urls.length)]
-  const res = http.get(url)
+  http.get(url)
   sleep(0.5);
 }
+
