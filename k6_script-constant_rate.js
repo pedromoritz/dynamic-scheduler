bcefd19d59884c8311b5312a9af5f17ac7cf3051
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-03-27 20:30:13 -0300

    update

---
+++ k6_script-constant_rate.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import { reportHTML } from "https://raw.githubusercontent.com/fziviello/k6-report-html/main/dist/reportHtml.min.js";
+import { getNormallyDistributedRandomNumber } from './pdf.js';
+
+export const options = {
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: parseInt(__ENV.VIRTUAL_USERS),
+      timeUnit: '1s',
+      duration: '30m',
+      preAllocatedVUs: 100,
+      maxVUs: 500,
+    },
+  },
+};
+
+const API_BASE_URL = `http://${__ENV.SVC_IP}`
+
+const urls = []
+
+for (let i = 0; i < parseInt(__ENV.POD_AMOUNT); i++) {
+  urls.push(`${API_BASE_URL}:31${String(i+1).padStart(3, '0')}`)
+}
+
+export function handleSummary(data) {
+  return {
+    [`results/summary_${__ENV.SCHEDULER_TYPE}_${__ENV.POD_AMOUNT}_${__ENV.VIRTUAL_USERS}.html`]: reportHTML(data, {title:`${__ENV.SCHEDULER_TYPE} - ${__ENV.POD_AMOUNT} pods - ${__ENV.VIRTUAL_USERS} maximum virtual users`})
+  };
+}
+
+export default function () {
+  const url = urls[getNormallyDistributedRandomNumber(urls.length)]
+  http.get(url)
+}
