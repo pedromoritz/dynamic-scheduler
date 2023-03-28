c2036346d2a294540c0dbfe039f7baac10961c0c
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-03-28 20:47:41 -0300

    update

--- k6_script-ramp.js
+++ k6_script-ramp.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { reportHTML } from "https://raw.githubusercontent.com/fziviello/k6-report-html/main/dist/reportHtml.min.js";
-import { getNormallyDistributedRandomNumber } from './pdf.js';
+import PD from './node_modules/probability-distributions-k6/index.js';
 import { sleep } from 'k6';
 
 export const options = {
@@ -32,8 +32,10 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const url = urls[getNormallyDistributedRandomNumber(urls.length)]
+  const rnorm_output = PD.rnorm(1, Math.round(urls.length / 2), 1)
+  const round_rnorm_output = Math.round(rnorm_output)
+  const limited_round_rnorm_output = Math.min(urls.length, Math.max(1, round_rnorm_output))
+  const url = urls[limited_round_rnorm_output - 1]
   http.get(url)
   sleep(0.5);
 }
-
