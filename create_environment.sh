2fde8ff772417e241bb5aa2852dd06ee803350db
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-03-29 10:35:33 -0300

    update

--- create_environment.sh
+++ create_environment.sh
@@ -2,4 +2,4 @@
 minikube stop -p ppgcc
 minikube delete -p ppgcc
 minikube start --nodes 4 -p ppgcc --cpus 2 --memory 2048 --disk-size 2G --driver=docker --kubernetes-version v1.26.1
-minikube addons enable metrics-server -p ppgcc
+kubectl apply -f metrics-server.yaml
