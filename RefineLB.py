5f1a580d01bbd1d973ead8a1336c809a5b15a786
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-06-02 20:50:40 -0300

    update

--- RefineLB.py
+++ RefineLB.py
@@ -7,26 +7,27 @@ import heapq
 import sys
 from functools import reduce
 
-CSV_FILENAME_BASE = sys.argv[1]+'_'+sys.argv[2]+'_'+sys.argv[3]+'.csv'
+CSV_FILENAME_BASE = sys.argv[1]+'_'+sys.argv[2]+'_'+sys.argv[3]+'_'+sys.argv[4]+'_'+sys.argv[5]+'_'+sys.argv[6]+'.csv'
 INTERVAL = 60
 COUNTER = 0
+METRIC = sys.argv[6]
 
 def get_refinelb_plan(processors):
   allocation_plan = {}
   heavyProcs = []
   lightProcs = []
   # calculating threshold
-  procs_memory_values = list(map(lambda n: n['usage']['memory'], processors))
-  procs_memory_average = round(reduce(lambda x, y: x + y, procs_memory_values) / len(procs_memory_values), 0) 
+  procs_values = list(map(lambda n: n['usage'][METRIC], processors))
+  procs_average = round(reduce(lambda x, y: x + y, procs_values) / len(procs_values), 0) 
   margin = 1.002 # >= 1.002
-  threshold = procs_memory_average * margin
+  threshold = procs_average * margin
   # defining heavyProcs and lightProcs based on threshold
   for processor in processors:
-    if int(processor['usage']['memory']) > threshold:
+    if int(processor['usage'][METRIC]) > threshold:
       heavyProcs.append(processor)
     else:
       lightProcs.append(processor)
-  heavyProcsMapped = list(map(lambda n: (n['usage']['memory'], n['name']), heavyProcs))
+  heavyProcsMapped = list(map(lambda n: (n['usage'][METRIC], n['name']), heavyProcs))
   heapq._heapify_max(heavyProcsMapped)
   finalProcs = []
   while len(heavyProcs) > 0:
@@ -34,9 +35,9 @@ def get_refinelb_plan(processors):
     while len(lightProcs) > 0:
       lightProc = lightProcs.pop()
       pods_from_donor = donor['pods']
-      pods_from_donor_sorted = list(map(lambda n: (n['usage']['memory'], n['name']), pods_from_donor))
+      pods_from_donor_sorted = list(map(lambda n: (n['usage'][METRIC], n['name']), pods_from_donor))
       donor_best_pod = pods_from_donor_sorted[-1]
-      if donor_best_pod[0] + lightProc['usage']['memory'] < procs_memory_average:
+      if donor_best_pod[0] + lightProc['usage'][METRIC] < procs_average:
         break
     donor['pods'] = [d for d in donor['pods'] if d['name'] != donor_best_pod[1]]
     finalProcs.append(donor)
