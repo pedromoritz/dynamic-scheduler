2e7039563641474a7ab8fc3adf3f5d0f81131f22
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-06-04 23:12:30 -0300

    update

--- standard_deviation_graphics.py
+++ standard_deviation_graphics.py
@@ -9,16 +9,36 @@ from pylab import cm
 import sys
 
 def save_graphic(value1, value2, value3, metric, filename):
-  if metric == 'memory':
+ # if metric == 'memory':
   #  plt.ylim(0, 500)
-    plt.ylabel('memory (MB)')
-  if metric == 'cpu':
+ #   plt.ylabel('memory (MB)')
+ # if metric == 'cpu':
   #  plt.ylim(0, 200)
-    plt.ylabel('CPU (millicpu)')
-  plt.bar('kube-scheduler', value1)
-  plt.bar('kse + GreedyLB', value2)
-  plt.bar('kse + RefineLB', value3)
+ #   plt.ylabel('CPU (millicpu)')
+#  plt.bar('', value1, label='kube-scheduler')
+#  plt.bar('', value2, label='kse + GreedyLB')
+#  plt.bar('', value3, label='kse + RefineLB')
   #plt.legend(loc="upper left")
+#  plt.savefig('results/'+filename, dpi=400, transparent=False, bbox_inches='tight')
+#  plt.close()
+#  plt.cla()
+#  plt.clf()
+
+  city=['Delhi','Beijing','Washington','Tokyo','Moscow']
+  Gender=['Male','Female']
+  pos = np.arange(len(city))
+  bar_width = 0.35
+  Happiness_Index_Male=[60,40,70,65,85]
+  Happiness_Index_Female=[30,60,70,55,75]
+ 
+  plt.bar(pos,Happiness_Index_Male,bar_width,color='blue',edgecolor='black')
+  plt.bar(pos+bar_width,Happiness_Index_Female,bar_width,color='pink',edgecolor='black')
+  plt.xticks(pos, city)
+  plt.xlabel('City', fontsize=16)
+  plt.ylabel('Happiness_Index', fontsize=16)
+  plt.title('Group Barchart - Happiness index across cities By Gender',fontsize=18)
+  plt.legend(Gender,loc=2)
+
   plt.savefig('results/'+filename, dpi=400, transparent=False, bbox_inches='tight')
   plt.close()
   plt.cla()
