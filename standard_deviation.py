9e0af14999f4cd91e58f032c0ff6d531df62577d
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-06-14 00:30:32 -0300

    update

--- standard_deviation.py
+++ standard_deviation.py
@@ -4,24 +4,23 @@ import os
 from pathlib import Path
 import numpy as np
 
+def f(number):
+  return str(round(number, 2))
+
 def get_standard_deviation(label, dataset):
   print('-------------------------------------------')
-  print(label)
   data_list = []
   for data in dataset:
     data_list.append(round(np.average(data), 2))
   sorted_data_list = sorted(data_list)
   min_value = sorted_data_list[0]
   max_value = sorted_data_list[-1]
-  print(' min: ' + str(round(min_value, 2)))
-  print(' max: ' + str(round(max_value, 2)))
   mean = sum(data_list) / len(data_list)
-  print('mean: ' + str(round(mean, 2)))
   variance = sum([((x - mean) ** 2) for x in data_list]) / len(data_list)
-  standard_deviation = round(variance ** 0.5, 2)
-  print('  sd: ' + str(standard_deviation))
+  standard_deviation = variance ** 0.5
+  print(label + ' --> min: ' + f(min_value) + ', max: ' + f(max_value) + ', mean: ' + f(mean) + ', sd: ' + f(standard_deviation))
 
-for path in Path("results/").glob("metrics_*.csv"):
+for path in sorted(Path("results/").glob("metrics_*.csv"), reverse=True):
   FILE_NAME, FILE_EXTENSION = os.path.splitext(path)
   timestamp, node1_pod_amount, node1_memory, node1_cpu, node2_pod_amount, node2_memory, node2_cpu, node3_pod_amount, node3_memory, node3_cpu, node4_pod_amount, node4_memory, node4_cpu = np.loadtxt(FILE_NAME + '.csv', unpack=True, delimiter=',', skiprows=1)
 
