3f444ecc1cb6ad5c2c5d09291281b4a9df9eed1e
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-06-22 10:47:37 -0300

    update

--- standard_deviation.py
+++ standard_deviation.py
@@ -24,36 +24,20 @@ for path in sorted(Path("results/").glob("metrics_*.csv"), reverse=True):
   FILE_NAME, FILE_EXTENSION = os.path.splitext(path)
   timestamp, node1_pod_amount, node1_memory, node1_cpu, node2_pod_amount, node2_memory, node2_cpu, node3_pod_amount, node3_memory, node3_cpu, node4_pod_amount, node4_memory, node4_cpu = np.loadtxt(FILE_NAME + '.csv', unpack=True, delimiter=',', skiprows=1)
 
-  if 'kube-scheduler' in FILE_NAME:
+  if 'memory' in FILE_NAME:
     # calculating memory standard deviation
     dataset = []
     dataset.append(list(map(lambda n: n/1024, node1_memory)))
     dataset.append(list(map(lambda n: n/1024, node2_memory)))
     dataset.append(list(map(lambda n: n/1024, node3_memory)))
     dataset.append(list(map(lambda n: n/1024, node4_memory)))
-    get_standard_deviation(FILE_NAME + "_memory", dataset)
+    get_standard_deviation(FILE_NAME, dataset)
+  elif 'cpu' in FILE_NAME:
     # calculating cpu standard deviation
     dataset = []
     dataset.append(list(map(lambda n: n/1000000, node1_cpu)))
     dataset.append(list(map(lambda n: n/1000000, node2_cpu)))
     dataset.append(list(map(lambda n: n/1000000, node3_cpu)))
     dataset.append(list(map(lambda n: n/1000000, node4_cpu)))
-    get_standard_deviation(FILE_NAME + "_cpu", dataset)
-  elif 'kse' in FILE_NAME:
-    if 'memory' in FILE_NAME:
-      # calculating memory standard deviation
-      dataset = []
-      dataset.append(list(map(lambda n: n/1024, node1_memory)))
-      dataset.append(list(map(lambda n: n/1024, node2_memory)))
-      dataset.append(list(map(lambda n: n/1024, node3_memory)))
-      dataset.append(list(map(lambda n: n/1024, node4_memory)))
-      get_standard_deviation(FILE_NAME, dataset)
-    elif 'cpu' in FILE_NAME:
-      # calculating cpu standard deviation
-      dataset = []
-      dataset.append(list(map(lambda n: n/1000000, node1_cpu)))
-      dataset.append(list(map(lambda n: n/1000000, node2_cpu)))
-      dataset.append(list(map(lambda n: n/1000000, node3_cpu)))
-      dataset.append(list(map(lambda n: n/1000000, node4_cpu)))
-      get_standard_deviation(FILE_NAME, dataset)
+    get_standard_deviation(FILE_NAME, dataset)
 
