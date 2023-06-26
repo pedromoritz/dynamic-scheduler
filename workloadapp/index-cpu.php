76d30b7829879d5b7a59cf9e8c01abc13da0410b
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-06-25 22:33:37 -0300

    update

--- workloadapp/index-cpu.php
+++ workloadapp/index-cpu.php
@@ -4,11 +4,11 @@ ini_set('memory_limit', '-1');
 ignore_user_abort(true);
 set_time_limit(0);
 
-$array = array();
-for ($i = 0; $i < 100000; $i++) {
-   array_push($array, md5(microtime(true).mt_Rand()));
+$sum = 0;
+for ($i = 0; $i < 1000; $i++) {
+   for ($j = 0; $j < 1000; $j++) {
+     $sum++;
+   } 
 }
 
-$used_mem = round(memory_get_usage(false) / 1024);
-$alloc_mem = round(memory_get_usage(true) / 1024);
-echo 'used: ' . $used_mem . 'KB, allocated: ' . $alloc_mem . 'KB';
+echo 'sum: ' . $sum;
