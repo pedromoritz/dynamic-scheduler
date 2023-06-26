ac9a80e62a5c210eb49c7ba9b808d89a51201875
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-06-25 23:10:33 -0300

    update

--- workloadapp/index-cpu.php
+++ workloadapp/index-cpu.php
@@ -5,8 +5,8 @@ ignore_user_abort(true);
 set_time_limit(0);
 
 $sum = 0;
-for ($i = 0; $i < 1000; $i++) {
-   for ($j = 0; $j < 1000; $j++) {
+for ($i = 0; $i < 10000; $i++) {
+   for ($j = 0; $j < 10000; $j++) {
      $sum++;
    } 
 }
