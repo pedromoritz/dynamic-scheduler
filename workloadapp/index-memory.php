4df6572d20b41f3068457f8c464f643d4ea01e13
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-06-28 21:21:12 -0300

    update

--- workloadapp/index-memory.php
+++ workloadapp/index-memory.php
@@ -6,7 +6,7 @@ set_time_limit(0);
 
 $array = array();
 for ($i = 0; $i < 100000; $i++) {
-   array_push($array, md5(microtime(true).mt_Rand())+microtime(time));
+   array_push($array, md5(microtime(true).mt_Rand()));
 }
 
 $used_mem = round(memory_get_usage(false) / 1024);
