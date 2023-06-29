4d0a8af6e8b6cd9614d43e788bc733bba551e577
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-06-29 11:46:58 -0300

    update

--- tests.sh
+++ tests.sh
@@ -10,61 +10,61 @@ clean()
 clean
 ./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=constant distribution=exponential metric=memory
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=constant distribution=exponential metric=memory
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=constant distribution=exponential metric=memory
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=constant distribution=exponential metric=memory
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=constant distribution=exponential metric=memory
 
 clean
-./run_kube-scheduler.sh pod_amount=20 target=40 rate_type=constant distribution=exponential metric=cpu
+./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=constant distribution=exponential metric=cpu
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=constant distribution=exponential metric=cpu
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=constant distribution=exponential metric=cpu
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=constant distribution=exponential metric=cpu
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=constant distribution=exponential metric=cpu
 
 # constant normal
 clean
-./run_kube-scheduler.sh pod_amount=20 target=40 rate_type=constant distribution=normal metric=memory
+./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=constant distribution=normal metric=memory
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=constant distribution=normal metric=memory
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=constant distribution=normal metric=memory
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=constant distribution=normal metric=memory
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=constant distribution=normal metric=memory
 
 clean
-./run_kube-scheduler.sh pod_amount=20 target=40 rate_type=constant distribution=normal metric=cpu
+./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=constant distribution=normal metric=cpu
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=constant distribution=normal metric=cpu
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=constant distribution=normal metric=cpu
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=constant distribution=normal metric=cpu
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=constant distribution=normal metric=cpu
 
 # ramp exponential
 clean
-./run_kube-scheduler.sh pod_amount=20 target=40 rate_type=ramp distribution=exponential metric=memory
+./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=ramp distribution=exponential metric=memory
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=ramp distribution=exponential metric=memory
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=ramp distribution=exponential metric=memory
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=ramp distribution=exponential metric=memory
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=ramp distribution=exponential metric=memory
 
 clean
-./run_kube-scheduler.sh pod_amount=20 target=40 rate_type=ramp distribution=exponential metric=cpu
+./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=ramp distribution=exponential metric=cpu
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=ramp distribution=exponential metric=cpu
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=ramp distribution=exponential metric=cpu
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=ramp distribution=exponential metric=cpu
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=ramp distribution=exponential metric=cpu
 
 # ramp normal
 clean
-./run_kube-scheduler.sh pod_amount=20 target=40 rate_type=ramp distribution=normal metric=memory
+./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=ramp distribution=normal metric=memory
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=ramp distribution=normal metric=memory
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=ramp distribution=normal metric=memory
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=ramp distribution=normal metric=memory
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=ramp distribution=normal metric=memory
 
 clean
-./run_kube-scheduler.sh pod_amount=20 target=40 rate_type=ramp distribution=normal metric=cpu
+./run_kube-scheduler.sh pod_amount=$1 target=$2 rate_type=ramp distribution=normal metric=cpu
 clean
-./run_kse.sh scheduler=GreedyLB pod_amount=20 target=40 rate_type=ramp distribution=normal metric=cpu
+./run_kse.sh scheduler=GreedyLB pod_amount=$1 target=$2 rate_type=ramp distribution=normal metric=cpu
 clean
-./run_kse.sh scheduler=RefineLB pod_amount=20 target=40 rate_type=ramp distribution=normal metric=cpu
+./run_kse.sh scheduler=RefineLB pod_amount=$1 target=$2 rate_type=ramp distribution=normal metric=cpu
 
 clean
 
