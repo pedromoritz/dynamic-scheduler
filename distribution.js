02b23e7fe86a134fbc03ae6460514e69ba0afda6
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-07-19 15:09:56 -0300

    update

--- distribution.js
+++ distribution.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 100000; i++) {
   let distribution_output = -1;
   while (distribution_output < 1 || distribution_output > pods) {
     // distribution_output = Math.round(PD.rexp(1, 5 / pods)); // exponential distribution
-    distribution_output = Math.round(PD.rnorm(1, (pods + 1) / 2, ((pods + 1) / 2) / 6)); // normal distribution
+    distribution_output = Math.round(PD.rnorm(1, (pods + 1) / 2, ((pods + 1) / 2) / 5)); // normal distribution
   }
 
   console.log(distribution_output - 1);
