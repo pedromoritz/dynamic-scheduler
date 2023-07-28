95613b169f8cecc8227213a3be742525320b91f4
Pedro Moritz de Carvalho Neto <pedro.neto@altoqi.com.br>
2023-07-28 18:51:37 -0300

    update

--- k6_script.js
+++ k6_script.js
@@ -1,18 +1,11 @@
-import http from 'k6/http';
+const PD = require('./node_modules/probability-distributions-k6/index.js');
 
-export const options = {
-  scenarios: {
-    constant_request_rate: {
-      executor: 'constant-arrival-rate',
-      rate: parseInt(__ENV.TA),
-      timeUnit: '1s',
-      duration: '12m',
-      preAllocatedVUs: 100,
-      maxVUs: 500,
-    },
-  },
-};
-
-export default function () {
-  http.get(`http://${__ENV.IP}:31001`);
+for (let i = 0; i < 24000; i++) {
+  let pods = 20;
+  let distribution_output = -1;
+  while (distribution_output < 1 || distribution_output > pods) {
+    //distribution_output = Math.round(PD.rexp(1, 5 / pods)); // exponential distribution
+    distribution_output = Math.round(PD.rnorm(1, (pods + 1) / 2, ((pods + 1) / 2) / 6)); // normal distribution
+  }
+  console.log(distribution_output - 1)
 }
