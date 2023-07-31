6a0cb23939f9bcd0b545ed7b523a3627027d4e11
Pedro Moritz de Carvalho Neto <pmoritz@yahoo.com>
2023-07-31 10:33:04 -0300

    update

--- create_environment.sh
+++ create_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 minikube stop -p ppgcc # stop cluster ppgcc
 minikube delete -p ppgcc # remove cluster ppgcc
-minikube start --nodes 1 -p ppgcc --cpus 2 --memory 2G --disk-size 5G --container-runtime containerd --vm --kubernetes-version v1.27.3 # add master node
+minikube start --nodes 1 -p ppgcc --cpus 2 --memory 2G --disk-size 10G --container-runtime containerd --vm --kubernetes-version v1.27.3 # add master node
 minikube addons enable metrics-server -p ppgcc # add metrics-server to master node
 sleep 180
 minikube node add -p ppgcc # add 1st worker node
